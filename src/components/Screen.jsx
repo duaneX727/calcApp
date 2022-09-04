5aae9e7384629f363765ed168893967df6d2a498
Marlon Mitchell <mdmitch7@gmail.com>
2022-09-04 16:01:14 -0400

    Added format for large numbers

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -4,8 +4,8 @@ import { CalcContext } from "../context/CalcContext";
 
 const Screen = () => {
   const {calc} = useContext(CalcContext);
-  return (
-    <Textfit className="screen" max={70} mode="single">{calc.num ? calc.num : calc.res}</Textfit>
+  return ( 
+    <Textfit className="screen" max={70} mode="single">{calc.num ? calc.num.toLocaleString() : calc.res.toLocaleString()}</Textfit>
   )
 }
 
